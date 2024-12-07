15f2da4183030cc27a1dd823da488382769b57a5
ConDavison1 <connor.e.davison@edu.sait.ca>
2024-12-07 13:54:38 -0700

    updated naming

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-purple-500 to-pink-500 text-white">
       <div className="text-center p-8">
-        <h1 className="text-5xl font-bold mb-6">Welcome to the Workout App!</h1>
-        <p className="text-xl mb-6">Find the perfect exercises to improve your fitness.</p>
+        <h1 className="text-5xl font-bold mb-6">Welcome to Fit Quest!</h1>
+        <p className="text-xl mb-6">Find and Track the perfect exercises to improve your fitness.</p>
         <Link
           href="/workouts" // Link to your Workouts page
           className="px-6 py-3 bg-purple-800 rounded text-white text-lg hover:bg-purple-600 transition duration-200"
