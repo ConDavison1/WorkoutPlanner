ab6ccae9926b63ae65737f532af567ffa71572f6
ConDavison1 <connor.e.davison@edu.sait.ca>
2024-12-08 14:54:49 -0700

    build errors

--- app/Website/_utils/auth-context.js
+++ app/Website/_utils/auth-context.js
@@ -1,12 +1,7 @@
-"use client";
 
+"use client";
 import { useContext, createContext, useState, useEffect } from "react";
-import {
-    signInWithPopup,
-    signOut,
-    onAuthStateChanged,
-    GithubAuthProvider,
-} from "firebase/auth";
+import { signInWithPopup, signOut, onAuthStateChanged, GithubAuthProvider } from "firebase/auth";
 import { auth } from "./firebase";
 
 const AuthContext = createContext();
@@ -14,16 +9,19 @@ const AuthContext = createContext();
 export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
-    const gitHubSignIn = () => {
+    const gitHubSignIn = async () => {
+        if (!auth) return;
         const provider = new GithubAuthProvider();
         return signInWithPopup(auth, provider);
     };
 
-    const firebaseSignOut = () => {
+    const firebaseSignOut = async () => {
+        if (!auth) return;
         return signOut(auth);
     };
 
     useEffect(() => {
+        if (!auth) return;
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
         });
